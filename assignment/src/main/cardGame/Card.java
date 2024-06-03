37ae93b1599ad8ce2e4010c8d6eeca23645b6ec5
jongheonleee <qwefghnm1212@gmail.com>
2024-06-03 20:43:45 +0900

    [refactor] 프로젝트 구조 개선

--- assignment/src/main/cardGame/Card.java
+++ assignment/src/main/cardGame/Card.java
@@ -1,8 +1,6 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+package main.cardGame;
+
+import java.util.*;
 
 
 // 카드 정보 저장
---
+++ assignment/src/test/cardGame/CardTest.java
@@ -0,0 +1,5 @@
+package test.cardGame;
+
+class CardTest {
+
+}
